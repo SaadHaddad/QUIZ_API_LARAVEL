79b72065efd6d8da02d1d3c5cd5850d9d970e112
haddad saad <saadhaddad1996@gmail.com>
2021-05-18 23:14:20 +0200

    ssss

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -21,7 +21,7 @@ class QuestionController extends Controller
 
     }
     public function allQuestion(){
-         $question = Questions::all();
+         $question = Questions::orderBy('score', 'DESC')->get();
         return response()->json($question);
 
     }
